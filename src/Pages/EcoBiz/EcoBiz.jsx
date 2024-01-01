2c7c8f3b45bede194ecba2769c13ff80fc341390
Sujith <p.sujithk@gmail.com>
2024-01-01 12:58:49 +0530

    Added EcoBiz

---
+++ src/Pages/EcoBiz/EcoBiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarEB from './NavbarEB';
+
+const Communiqa = () => {
+  return (
+    <div>
+      <NavbarEB />
+    </div>
+  )
+}
+
+export default Communiqa;
