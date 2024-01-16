04c5508eb6a502b28ae830e877787bedcd53054b
Sujith <p.sujithk@gmail.com>
2024-01-16 20:55:59 +0530

    added modal and made some changes to calendar

--- components/EventCalendar/EventCalendar.jsx
+++ components/EventCalendar/EventCalendar.jsx
@@ -3,19 +3,19 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
+import './EventCalendar.css'; // Import a CSS file for styling
+
 const EventCalendar = () => {
   const [events, setEvents] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [eventTitle, setEventTitle] = useState('');
+  
 
   useEffect(() => {
     // Fetch events from the server when the component mounts
     fetchEvents();
   }, []);
-
-  useEffect(() => {
-    // This effect will run whenever 'events' state is updated
-    // You can perform any additional logic here
-    console.log('Events updated:', events);
-  }, [events]);
 
   const fetchEvents = async () => {
     try {
@@ -38,9 +38,19 @@ const EventCalendar = () => {
     }
   };
 
-  const handleDateSelect = async (arg) => {
-    const title = prompt('Enter event title:');
-    if (title) {
+  const openModal = (arg) => {
+    setSelectedDate(arg.startStr);
+    setEventTitle('');
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    location.reload();
+  };
+
+  const handleDateSelect = async () => {
+    if (eventTitle) {
       try {
         const response = await fetch('http://localhost:3001/events', {
           method: 'POST',
@@ -48,42 +58,32 @@ const EventCalendar = () => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            title: title,
-            event_date: arg.startStr,
+            title: eventTitle,
+            event_date: selectedDate,
           }),
         });
 
         if (response.ok) {
           const newEvent = await response.json();
           setEvents((prevEvents) => [...prevEvents, newEvent]);
-          window.location.reload();
         } else {
           console.error('Failed to insert event on the server');
         }
       } catch (error) {
         console.error('Error during event insertion:', error);
+      } finally {
+        closeModal();
       }
     }
   };
 
-  const handleEventClick = async (info) => {
-    const action = prompt('Do you want to delete or modify the event? (Type "delete" or "modify"):');
+  const handleEventUpdate = async (info, e) => {
+    e.stopPropagation(); // Stop event propagation
 
-    if (action === 'delete') {
-      const response = await fetch(`http://localhost:3001/events/${info.event.id}`, {
-        method: 'DELETE',
-      });
 
-      if (response.ok) {
-        const updatedEvents = events.filter((event) => event.id !== info.event.id);
-        setEvents(updatedEvents);
-        window.location.reload();
-      } else {
-        console.error('Failed to delete event on the server');
-      }
-    } else if (action === 'modify') {
-      const newTitle = prompt('Enter new event title:');
-      if (newTitle) {
+    const newTitle = prompt('Enter new event title:');
+    if (newTitle) {
+      try {
         const response = await fetch(`http://localhost:3001/events/${info.event.id}`, {
           method: 'PUT',
           headers: {
@@ -100,11 +100,39 @@ const EventCalendar = () => {
           );
 
           setEvents(updatedEvents);
-          window.location.reload();
+          location.reload();
+
         } else {
           console.error('Failed to update event on the server');
         }
+      } catch (error) {
+        console.error('Error during event update:', error);
       }
+    }
+  };
+
+  const handleEventDelete = async (info, e) => {
+    e.stopPropagation(); // Stop event propagation
+    
+    const shouldDelete = window.confirm('Are you sure you want to delete this event?');
+    if (!shouldDelete) {
+      return;
+    }
+  
+    try {
+      const response = await fetch(`http://localhost:3001/events/${info.event.id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        const updatedEvents = events.filter((event) => event.id !== info.event.id);
+        setEvents(updatedEvents);
+        location.reload();
+      } else {
+        console.error('Failed to delete event on the server');
+      }
+    } catch (error) {
+      console.error('Error during event deletion:', error);
     }
   };
 
@@ -112,22 +140,63 @@ const EventCalendar = () => {
     plugins: [dayGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
     headerToolbar: {
-      start: 'title',
-      center: '',
-      end: 'dayGridMonth,prev,next',
+      start: 'prev',
+      center: 'title',
+      end: 'next',
     },
     selectable: true,
     height: '100vh',
-    select: handleDateSelect,
+    select: openModal,
     events: events,
-    eventClick: handleEventClick,
     displayEventTime: false,
     eventDisplay: 'block',
+    eventClick: handleEventUpdate,
+    eventMouseEnter: (info) => (info.el.style.cursor = 'pointer'),
+    eventMouseLeave: (info) => (info.el.style.cursor = ''),
+    eventContent: (info) => {
+      const deleteButton = document.createElement('span');
+      deleteButton.innerHTML = ' &#x2716;';
+      deleteButton.className = 'event-delete-button';
+      deleteButton.addEventListener('click', (e) => handleEventDelete(info, e));
+  
+      const updateButton = document.createElement('span');
+      updateButton.innerHTML = ' &#x270E;';
+      updateButton.className = 'event-update-button';
+      updateButton.addEventListener('click', (e) => handleEventUpdate(info, e));
+  
+      const titleContainer = document.createElement('div');
+      titleContainer.className = 'event-title';
+      titleContainer.innerHTML = `${info.event.title}`;
+  
+      const container = document.createElement('div');
+      container.className = 'event-content-container';
+      container.appendChild(titleContainer);
+      container.appendChild(deleteButton);
+      container.appendChild(updateButton);
+  
+      return { domNodes: [container] };
+    },
   };
 
   return (
-    <div className='event_calendar'>
+     <div className='event_calendar'>
       <FullCalendar key={JSON.stringify(events)} {...calendarOptions} />
+
+      {/* Modal for date selection */}
+      {isModalOpen && (
+        <div className="custom-modal-overlay" onClick={closeModal}>
+          <div className="custom-modal" onClick={(e) => e.stopPropagation()}>
+            <p>Enter event title:</p>
+            <input
+              type="text"
+              id="eventTitleInput"
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)}
+            />
+            <button onClick={handleDateSelect}>Save</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
