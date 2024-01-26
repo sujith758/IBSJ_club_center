fae5e21981f5cf63418886ba12691dae0ecebeb3
Sujith <p.sujithk@gmail.com>
2024-01-26 19:41:09 +0530

    changes in lottie animations

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import Lottie from "lottie-react";
-import FloatingThings from "../../public/FloatingHomepage.json";
+import ShootingString from "../../public/ShootingString.json";
 import "./Homepage.css";
 import ScrollMenu from "./ScrollMenu/ScrollMenu";
 import NavbarHP from "./Navbar/NavbarHP";
@@ -50,52 +50,54 @@ const Homepage = () => {
         ease: "power1.inOut",
       }
     );
-    },[]);
-    useEffect(() => {
-    // Control Lottie animation based on scroll position
-    gsap.to(".waving-hand", {
-      y: scrollPosition * 0.2, // Adjust the factor as needed
-      ease: "none",
+  }, []);
+  useEffect(() => {
+    gsap.from(".floating-lottie", {
+      x: -1000,
+      duration: 5,
     });
-  }, [scrollPosition]);
+    gsap.to(".floating-lottie", {
+      x: 1200, // Adjust the factor as needed
+      duration: 10,
+    });
+  }, []);
 
   return (
     <m.main
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 0.75, ease: "easeInOut" }}
-    
-  >
-    <div className="homepage__div">
-      <NavbarHP />
-      <div className="floating-lottie">
-        <Lottie
-          animationData={FloatingThings}
-          loop={true}
-          className="waving-hand"
-        />
-      </div>
-      <div className="content-homepage">
-        <div className="to-center">
-          <div className="homepage_title">
-            <h1>IBS JAIPUR</h1>
-            <h1>CLUB CENTER</h1>
-          </div>
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.75, ease: "easeInOut" }}
+    >
+      <div className="homepage__div">
+        <NavbarHP />
+        <div className="floating-lottie">
+          <Lottie
+            animationData={ShootingString}
+            loop={true}
+            className="shooting-star"
+          />
         </div>
-        {/* <div className="homepage_title">
+        <div className="content-homepage">
+          <div className="to-center">
+            <div className="homepage_title">
+              <h1>IBS JAIPUR</h1>
+              <h1>CLUB CENTER</h1>
+            </div>
+          </div>
+          {/* <div className="homepage_title">
           <h1>IBS JAIPUR</h1>
           <h1>CLUB CENTER</h1>
         </div> */}
-        <div className="bodybottom-content" ref={BodyBottomContentRef}>
-          <p>WELCOME TO</p>
-          <p>IBS JAIPUR CLUB CENTER</p>
-          <p>FUN BEGINS HERE</p>
-          <div className="down-arrow">&#9660;</div>
+          <div className="bodybottom-content" ref={BodyBottomContentRef}>
+            <p>WELCOME TO</p>
+            <p>IBS JAIPUR CLUB CENTER</p>
+            <p>FUN BEGINS HERE</p>
+            <div className="down-arrow">&#9660;</div>
+          </div>
         </div>
+        <h2>CLUBS</h2>
+        <ScrollMenu style={{ position: "relative" }} />
       </div>
-      <h2>CLUBS</h2>
-      <ScrollMenu style={{ position: "relative" }} />
-    </div>
     </m.main>
   );
 };
